5e60db09a7af4899f5a1935f0b43e27e966cb68c
Vladislav Smolin <vlsmol@taltech.ee>
2025-02-12 16:29:40 +0200

    asd

---
+++ PrjTekstiPoorajaKomponent/ITeisendaja.vb
@@ -0,0 +1,10 @@
+﻿Public Interface ITeisendaja
+    Property EsimeseTaheAsciiKood As Integer
+
+    Property ViimaseTaheAsciiKood As Integer
+
+    Property Tekst As String
+    Function PooraTekst() As String
+
+    Sub TeisendajaTekst(ByRef sisendTekst As String)
+End Interface
