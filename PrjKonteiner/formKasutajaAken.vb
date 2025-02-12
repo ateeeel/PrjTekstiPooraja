b8b1382954c71ae147aed5f5ef6e617626d75b4f
Vladislav Smolin <vlsmol@taltech.ee>
2025-02-12 17:24:43 +0200

    sasd

--- PrjKonteiner/formKasutajaAken.vb
+++ PrjKonteiner/formKasutajaAken.vb
@@ -1,5 +1,13 @@
 ﻿Public Class formKasutajaAken
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
+    Private Sub btnPooraFunktsiooniga_Click(sender As Object, e As EventArgs) _
+        Handles btnPooraFunktsiooniga.Click
+        Dim pooraja As PrjTekstiPoorajaKomponent.ITeisendaja
+
+        pooraja = New PrjTekstiPoorajaKomponent.TekstiPooraja
+
+        pooraja.Tekst = txtSisendTekst.Text
+
+        txtValjundTekst1.Text = pooraja.PooraTekst()
     End Sub
 End Class
