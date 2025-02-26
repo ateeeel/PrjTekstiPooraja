69d9fbd10ab7c99ef88263374347069eb29a808d
Vladislav Smolin <vlsmol@taltech.ee>
2025-02-26 18:49:59 +0200

    asdasd

--- PrjKonteiner/formKasutajaAken.Designer.vb
+++ PrjKonteiner/formKasutajaAken.Designer.vb
@@ -22,176 +22,179 @@ Partial Class formKasutajaAken
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.components = New System.ComponentModel.Container()
-        Me.txtSisendTekst = New System.Windows.Forms.TextBox()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.txtValjundTekst1 = New System.Windows.Forms.TextBox()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.btnPooraFunktsiooniga = New System.Windows.Forms.Button()
-        Me.btnPooraProtseduuriga = New System.Windows.Forms.Button()
-        Me.txtValjundTekst2 = New System.Windows.Forms.TextBox()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.btnStart = New System.Windows.Forms.Button()
-        Me.btnStopp = New System.Windows.Forms.Button()
-        Me.timerUuenda = New System.Windows.Forms.Timer(Me.components)
-        Me.charLength = New System.Windows.Forms.Label()
-        Me.lblVowels = New System.Windows.Forms.Label()
-        Me.lblAscii = New System.Windows.Forms.Label()
-        Me.cmbPoorajaValik = New System.Windows.Forms.ComboBox()
-        Me.SuspendLayout()
-        '
-        'txtSisendTekst
-        '
-        Me.txtSisendTekst.Location = New System.Drawing.Point(415, 177)
-        Me.txtSisendTekst.Name = "txtSisendTekst"
-        Me.txtSisendTekst.Size = New System.Drawing.Size(419, 26)
-        Me.txtSisendTekst.TabIndex = 0
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(316, 180)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(93, 20)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Sisendtekst"
-        '
-        'txtValjundTekst1
-        '
-        Me.txtValjundTekst1.Location = New System.Drawing.Point(415, 250)
-        Me.txtValjundTekst1.Name = "txtValjundTekst1"
-        Me.txtValjundTekst1.Size = New System.Drawing.Size(419, 26)
-        Me.txtValjundTekst1.TabIndex = 2
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(225, 253)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(184, 20)
-        Me.Label2.TabIndex = 3
-        Me.Label2.Text = "Pooramine funktsiooniga"
-        '
-        'btnPooraFunktsiooniga
-        '
-        Me.btnPooraFunktsiooniga.Location = New System.Drawing.Point(524, 296)
-        Me.btnPooraFunktsiooniga.Name = "btnPooraFunktsiooniga"
-        Me.btnPooraFunktsiooniga.Size = New System.Drawing.Size(188, 47)
-        Me.btnPooraFunktsiooniga.TabIndex = 4
-        Me.btnPooraFunktsiooniga.Text = "Poora funktsiooniga"
-        Me.btnPooraFunktsiooniga.UseVisualStyleBackColor = True
-        '
-        'btnPooraProtseduuriga
-        '
-        Me.btnPooraProtseduuriga.Location = New System.Drawing.Point(524, 432)
-        Me.btnPooraProtseduuriga.Name = "btnPooraProtseduuriga"
-        Me.btnPooraProtseduuriga.Size = New System.Drawing.Size(188, 48)
-        Me.btnPooraProtseduuriga.TabIndex = 5
-        Me.btnPooraProtseduuriga.Text = "Poora protseduuriga"
-        Me.btnPooraProtseduuriga.UseVisualStyleBackColor = True
-        '
-        'txtValjundTekst2
-        '
-        Me.txtValjundTekst2.Location = New System.Drawing.Point(415, 371)
-        Me.txtValjundTekst2.Name = "txtValjundTekst2"
-        Me.txtValjundTekst2.Size = New System.Drawing.Size(419, 26)
-        Me.txtValjundTekst2.TabIndex = 6
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(222, 371)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(187, 20)
-        Me.Label3.TabIndex = 7
-        Me.Label3.Text = "Pooramine protseduuriga"
-        '
-        'btnStart
-        '
-        Me.btnStart.Location = New System.Drawing.Point(308, 622)
-        Me.btnStart.Name = "btnStart"
-        Me.btnStart.Size = New System.Drawing.Size(101, 45)
-        Me.btnStart.TabIndex = 8
-        Me.btnStart.Text = "Start"
-        Me.btnStart.UseVisualStyleBackColor = True
-        '
-        'btnStopp
-        '
-        Me.btnStopp.Location = New System.Drawing.Point(566, 621)
-        Me.btnStopp.Name = "btnStopp"
-        Me.btnStopp.RightToLeft = System.Windows.Forms.RightToLeft.No
-        Me.btnStopp.Size = New System.Drawing.Size(84, 46)
-        Me.btnStopp.TabIndex = 9
-        Me.btnStopp.Text = "Stop"
-        Me.btnStopp.UseVisualStyleBackColor = True
-        '
-        'timerUuenda
-        '
-        Me.timerUuenda.Interval = 3000
-        '
-        'charLength
-        '
-        Me.charLength.AutoSize = True
-        Me.charLength.Location = New System.Drawing.Point(863, 180)
-        Me.charLength.Name = "charLength"
-        Me.charLength.Size = New System.Drawing.Size(59, 20)
-        Me.charLength.TabIndex = 10
-        Me.charLength.Text = "Pikkus:"
-        Me.charLength.UseWaitCursor = True
-        '
-        'lblVowels
-        '
-        Me.lblVowels.AutoSize = True
-        Me.lblVowels.Location = New System.Drawing.Point(864, 141)
-        Me.lblVowels.Name = "lblVowels"
-        Me.lblVowels.Size = New System.Drawing.Size(64, 20)
-        Me.lblVowels.TabIndex = 11
-        Me.lblVowels.Text = "Vowels:"
-        '
-        'lblAscii
-        '
-        Me.lblAscii.AutoSize = True
-        Me.lblAscii.Location = New System.Drawing.Point(863, 218)
-        Me.lblAscii.Name = "lblAscii"
-        Me.lblAscii.Size = New System.Drawing.Size(130, 40)
-        Me.lblAscii.TabIndex = 12
-        Me.lblAscii.Text = "First ASCII char: " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Last ASCII char:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
-        '
-        'cmbPoorajaValik
-        '
-        Me.cmbPoorajaValik.FormattingEnabled = True
-        Me.cmbPoorajaValik.Location = New System.Drawing.Point(868, 580)
-        Me.cmbPoorajaValik.Name = "cmbPoorajaValik"
-        Me.cmbPoorajaValik.Size = New System.Drawing.Size(195, 28)
-        Me.cmbPoorajaValik.TabIndex = 13
-        '
-        'formKasutajaAken
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1383, 742)
-        Me.Controls.Add(Me.cmbPoorajaValik)
-        Me.Controls.Add(Me.lblAscii)
-        Me.Controls.Add(Me.lblVowels)
-        Me.Controls.Add(Me.charLength)
-        Me.Controls.Add(Me.btnStopp)
-        Me.Controls.Add(Me.btnStart)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.txtValjundTekst2)
-        Me.Controls.Add(Me.btnPooraProtseduuriga)
-        Me.Controls.Add(Me.btnPooraFunktsiooniga)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.txtValjundTekst1)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.txtSisendTekst)
-        Me.Name = "formKasutajaAken"
-        Me.Text = "Tekstitöötlus"
-        Me.ResumeLayout(False)
-        Me.PerformLayout()
+		Me.components = New System.ComponentModel.Container()
+		Me.txtSisendTekst = New System.Windows.Forms.TextBox()
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.txtValjundTekst1 = New System.Windows.Forms.TextBox()
+		Me.Label2 = New System.Windows.Forms.Label()
+		Me.btnPooraFunktsiooniga = New System.Windows.Forms.Button()
+		Me.btnPooraProtseduuriga = New System.Windows.Forms.Button()
+		Me.txtValjundTekst2 = New System.Windows.Forms.TextBox()
+		Me.Label3 = New System.Windows.Forms.Label()
+		Me.btnStart = New System.Windows.Forms.Button()
+		Me.btnStopp = New System.Windows.Forms.Button()
+		Me.timerUuenda = New System.Windows.Forms.Timer(Me.components)
+		Me.charLength = New System.Windows.Forms.Label()
+		Me.lblVowels = New System.Windows.Forms.Label()
+		Me.lblAscii = New System.Windows.Forms.Label()
+		Me.cmbPoorajaValik = New System.Windows.Forms.ComboBox()
+		Me.SuspendLayout()
+		'
+		'txtSisendTekst
+		'
+		Me.txtSisendTekst.Location = New System.Drawing.Point(415, 177)
+		Me.txtSisendTekst.Name = "txtSisendTekst"
+		Me.txtSisendTekst.Size = New System.Drawing.Size(419, 26)
+		Me.txtSisendTekst.TabIndex = 0
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.Location = New System.Drawing.Point(316, 180)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(93, 20)
+		Me.Label1.TabIndex = 1
+		Me.Label1.Text = "Sisendtekst"
+		'
+		'txtValjundTekst1
+		'
+		Me.txtValjundTekst1.Location = New System.Drawing.Point(415, 250)
+		Me.txtValjundTekst1.Name = "txtValjundTekst1"
+		Me.txtValjundTekst1.ReadOnly = True
+		Me.txtValjundTekst1.Size = New System.Drawing.Size(419, 26)
+		Me.txtValjundTekst1.TabIndex = 2
+		'
+		'Label2
+		'
+		Me.Label2.AutoSize = True
+		Me.Label2.Location = New System.Drawing.Point(225, 253)
+		Me.Label2.Name = "Label2"
+		Me.Label2.Size = New System.Drawing.Size(184, 20)
+		Me.Label2.TabIndex = 3
+		Me.Label2.Text = "Pooramine funktsiooniga"
+		'
+		'btnPooraFunktsiooniga
+		'
+		Me.btnPooraFunktsiooniga.Location = New System.Drawing.Point(524, 296)
+		Me.btnPooraFunktsiooniga.Name = "btnPooraFunktsiooniga"
+		Me.btnPooraFunktsiooniga.Size = New System.Drawing.Size(188, 47)
+		Me.btnPooraFunktsiooniga.TabIndex = 4
+		Me.btnPooraFunktsiooniga.Text = "Poora funktsiooniga"
+		Me.btnPooraFunktsiooniga.UseVisualStyleBackColor = True
+		'
+		'btnPooraProtseduuriga
+		'
+		Me.btnPooraProtseduuriga.Location = New System.Drawing.Point(524, 432)
+		Me.btnPooraProtseduuriga.Name = "btnPooraProtseduuriga"
+		Me.btnPooraProtseduuriga.Size = New System.Drawing.Size(188, 48)
+		Me.btnPooraProtseduuriga.TabIndex = 5
+		Me.btnPooraProtseduuriga.Text = "Poora protseduuriga"
+		Me.btnPooraProtseduuriga.UseVisualStyleBackColor = True
+		'
+		'txtValjundTekst2
+		'
+		Me.txtValjundTekst2.Location = New System.Drawing.Point(415, 371)
+		Me.txtValjundTekst2.Name = "txtValjundTekst2"
+		Me.txtValjundTekst2.ReadOnly = True
+		Me.txtValjundTekst2.Size = New System.Drawing.Size(419, 26)
+		Me.txtValjundTekst2.TabIndex = 6
+		'
+		'Label3
+		'
+		Me.Label3.AutoSize = True
+		Me.Label3.Location = New System.Drawing.Point(222, 371)
+		Me.Label3.Name = "Label3"
+		Me.Label3.Size = New System.Drawing.Size(187, 20)
+		Me.Label3.TabIndex = 7
+		Me.Label3.Text = "Pooramine protseduuriga"
+		'
+		'btnStart
+		'
+		Me.btnStart.Location = New System.Drawing.Point(308, 622)
+		Me.btnStart.Name = "btnStart"
+		Me.btnStart.Size = New System.Drawing.Size(101, 45)
+		Me.btnStart.TabIndex = 8
+		Me.btnStart.Text = "Start"
+		Me.btnStart.UseVisualStyleBackColor = True
+		'
+		'btnStopp
+		'
+		Me.btnStopp.Enabled = False
+		Me.btnStopp.Location = New System.Drawing.Point(566, 621)
+		Me.btnStopp.Name = "btnStopp"
+		Me.btnStopp.RightToLeft = System.Windows.Forms.RightToLeft.No
+		Me.btnStopp.Size = New System.Drawing.Size(84, 46)
+		Me.btnStopp.TabIndex = 9
+		Me.btnStopp.Text = "Stop"
+		Me.btnStopp.UseVisualStyleBackColor = True
+		'
+		'timerUuenda
+		'
+		Me.timerUuenda.Interval = 3000
+		'
+		'charLength
+		'
+		Me.charLength.AutoSize = True
+		Me.charLength.Location = New System.Drawing.Point(863, 180)
+		Me.charLength.Name = "charLength"
+		Me.charLength.Size = New System.Drawing.Size(59, 20)
+		Me.charLength.TabIndex = 10
+		Me.charLength.Text = "Pikkus:"
+		Me.charLength.UseWaitCursor = True
+		'
+		'lblVowels
+		'
+		Me.lblVowels.AutoSize = True
+		Me.lblVowels.Location = New System.Drawing.Point(864, 141)
+		Me.lblVowels.Name = "lblVowels"
+		Me.lblVowels.Size = New System.Drawing.Size(64, 20)
+		Me.lblVowels.TabIndex = 11
+		Me.lblVowels.Text = "Vowels:"
+		'
+		'lblAscii
+		'
+		Me.lblAscii.AutoSize = True
+		Me.lblAscii.Location = New System.Drawing.Point(863, 218)
+		Me.lblAscii.Name = "lblAscii"
+		Me.lblAscii.Size = New System.Drawing.Size(130, 40)
+		Me.lblAscii.TabIndex = 12
+		Me.lblAscii.Text = "First ASCII char: " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Last ASCII char:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+		'
+		'cmbPoorajaValik
+		'
+		Me.cmbPoorajaValik.FormattingEnabled = True
+		Me.cmbPoorajaValik.Location = New System.Drawing.Point(868, 580)
+		Me.cmbPoorajaValik.Name = "cmbPoorajaValik"
+		Me.cmbPoorajaValik.Size = New System.Drawing.Size(195, 28)
+		Me.cmbPoorajaValik.TabIndex = 13
+		'
+		'formKasutajaAken
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(1383, 742)
+		Me.Controls.Add(Me.cmbPoorajaValik)
+		Me.Controls.Add(Me.lblAscii)
+		Me.Controls.Add(Me.lblVowels)
+		Me.Controls.Add(Me.charLength)
+		Me.Controls.Add(Me.btnStopp)
+		Me.Controls.Add(Me.btnStart)
+		Me.Controls.Add(Me.Label3)
+		Me.Controls.Add(Me.txtValjundTekst2)
+		Me.Controls.Add(Me.btnPooraProtseduuriga)
+		Me.Controls.Add(Me.btnPooraFunktsiooniga)
+		Me.Controls.Add(Me.Label2)
+		Me.Controls.Add(Me.txtValjundTekst1)
+		Me.Controls.Add(Me.Label1)
+		Me.Controls.Add(Me.txtSisendTekst)
+		Me.Name = "formKasutajaAken"
+		Me.Text = "Tekstitöötlus"
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
 
-    End Sub
+	End Sub
 
-    Friend WithEvents txtSisendTekst As TextBox
+	Friend WithEvents txtSisendTekst As TextBox
     Friend WithEvents Label1 As Label
     Friend WithEvents txtValjundTekst1 As TextBox
     Friend WithEvents Label2 As Label
